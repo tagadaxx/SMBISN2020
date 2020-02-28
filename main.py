2ddce31b68008b3d2a881a59dd252f3249b6e57a
Louis-Marie <blep.doge@gmail.com>
2020-02-28 20:01:59 +0100

    V0.1
    urf

--- main.py
+++ main.py
@@ -12,10 +12,10 @@ etat = RUN
 
 fond = pygame.image.load("map.png").convert_alpha()
 fond = pygame.transform.scale(fond, (10176,672))
-
+walk = 0
 
 pygame.key.set_repeat(100, 25)
-mario = perso("SuperMarioWalk3.gif", 64, 512)
+mario = perso("MarioSmall.gif", 64, 544)
 MarioState = 0
 while continuer:
 
@@ -31,11 +31,29 @@ while continuer:
                 mario = perso("mariocrouch.gif", mario.x, mario.y)
             if event.key == K_LEFT:
                 mario.x += -10
+                if walk == 0 :
+                    mario = perso("MarioSmallWalk1.gif", mario.x, mario.y)
+                    walk += 1
+                if walk == 1:
+                    mario = perso("MarioSmallWalk2.gif", mario.x, mario.y)
+                    walk += 1
+                if walk == 2:
+                    mario = perso("MarioSmallWalk3.gif", mario.x, mario.y)
+                    walk = 0
             if event.key == K_RIGHT:
                 mario.x += 10
+                if walk == 0 :
+                    mario = perso("MarioSmallWalk1.gif", mario.x, mario.y)
+                    walk += 1
+                if walk == 1:
+                    mario = perso("MarioSmallWalk2.gif", mario.x, mario.y)
+                    walk += 1
+                if walk == 2:
+                    mario = perso("MarioSmallWalk3.gif", mario.x, mario.y)
+                    walk = 0
         if event.type == KEYUP :
-            if event.key == K_DOWN :
-                mario = perso("SuperMarioWalk3.gif", mario.x, mario.y)
+            if event.key == K_DOWN or event.key == K_LEFT or event.key == K_RIGHT:
+                mario = perso("MarioSmall.gif", mario.x, mario.y)
 
 
 
