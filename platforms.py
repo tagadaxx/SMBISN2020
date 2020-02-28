b84f4386a91029b1cde869f0ef67600f1a4dc3d6
Louis-Marie <blep.doge@gmail.com>
2020-02-29 00:55:03 +0100

    BONNE VERSION

--- platforms.py
+++ platforms.py
@@ -1,11 +1,10 @@
 import pygame
 
 class Platform(pygame.sprite.Sprite):
-# x location, y location, img width, img height, img file    
+# x location, y location, img width, img height, img file
     def __init__(self,x,y,imgw,imgh,img):
         pygame.sprite.Sprite.__init__(self)
         self.image = pygame.image.load(img).convert_alpha()
-        self.image.convert_alpha()
         self.rect = self.image.get_rect()
         self.rect.y = y
         self.rect.x = x
