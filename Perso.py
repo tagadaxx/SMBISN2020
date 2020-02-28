4c37094facaa110600150c7d6cf539943acb364a
Louis-Marie <blep.doge@gmail.com>
2020-02-28 17:40:22 +0100

    maineqf

---
+++ Perso.py
@@ -0,0 +1,27 @@
+class perso():
+    def __init__(self, image, x=0, y=0, directionX=1, directionY=1):
+        self.image = pygame.image.load(image).convert_alpha()
+        self.x = x
+        self.y = y
+        self.directionX = directionX
+        self.directionY = directionY
+
+    def vie(,mavie, x=50, y=50):
+      mavie = 5
+     if perdu
+     mavie = mavie-1
+     print(mavie)
+
+
+
+    def move(self):
+        if self.x == 0:
+            self.directionX = 1
+        if self.x == screensize[0] - 60:
+            self.directionX = -1
+        if self.y == 0:
+            self.directionY = 1
+        if self.y == screensize[1] - 100:
+            self.directionY = -1
+        self.x += self.directionX
+        self.y += self.directionY
--- main.py
+++ main.py
@@ -1,1 +1,21 @@
-nbla
+import pygame
+from pygame.locals import *
+
+screensize = (1280, 720)
+pygame.init()
+fenetre = pygame.display.set_mode(screensize, RESIZABLE)
+
+continuer = True
+RUN, PAUSE = 0, 1
+etat = RUN
+
+fond = pygame.image.load("overworld_bg.png").convert_alpha()
+
+while continuer:
+    for event in pygame.event.get():
+
+        if event.type == QUIT:
+            continuer = False
+            quit()
+
+    fenetre.blit(fond, (0, 0))
