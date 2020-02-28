2ce22df750316e762f54c4a59050037839ac4d63
smansteel <smansteel@users.noreply.github.com>
2020-02-28 20:10:07 +0100

    V111

--- main.py
+++ main.py
@@ -5,11 +5,11 @@ from pygame.locals import *
 screensize = (897, 672)
 pygame.init()
 fenetre = pygame.display.set_mode(screensize, RESIZABLE)
-
+jump, xaya = 0, -3
 continuer = True
 RUN, PAUSE = 0, 1
 etat = RUN
-
+initjump = 0
 fond = pygame.image.load("map.png").convert_alpha()
 fond = pygame.transform.scale(fond, (10176,672))
 
@@ -36,9 +36,18 @@ while continuer:
         if event.type == KEYUP :
             if event.key == K_DOWN :
                 mario = perso("SuperMarioWalk3.gif", mario.x, mario.y)
-
-
-
+                mario.x += 5
+                
+    if jump == 1 and xaya<5 :
+        if initjump == 0 :
+            xaya = -5
+            initjump = 1
+        else :
+            xaya += 1
+        mario.y = round(-2/3*(xaya**2), 0)
+    else :
+        initjump = 0
+        jump = 0
 
     fenetre.blit(fond, (0, 0))
     fenetre.blit(mario.image, (mario.x,mario.y))
